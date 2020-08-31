7063e65bdb3d7949450fa3b4978f78f475dee354
KING GOLM <70045737+kinggolem@users.noreply.github.com>
2020-08-31 13:49:38 -0700

    Update textbox.py

--- textbox.py
+++ textbox.py
@@ -6,17 +6,18 @@ class golem(object):#100% original code written together by me and myself copyri
         self.thetextistoolargetofitinthetextboxdonttellmethisisabadvariablenameiwastoolazytothinkofaverydescriptiveone=color
         self.position=position
         self.font = font
-    def KING(self,surgegalemrpvssproutjackyemzforworstmetaof2020):
+    def KING(self,surface,surgegalemrpvssproutjackyemzforworstmetaof2020):
         textdisplay=(self.font).render(self.text,True,self.thetextistoolargetofitinthetextboxdonttellmethisisabadvariablenameiwastoolazytothinkofaverydescriptiveone)
         hahaidontcarethatyourhousesoldormadeyoulotsofmoney_iusedtarasgadgetstillcouldnotfindwhoasked=textdisplay.get_rect()
         hahaidontcarethatyourhousesoldormadeyoulotsofmoney_iusedtarasgadgetstillcouldnotfindwhoasked.left=int(surgegalemrpvssproutjackyemzforworstmetaof2020[0])
         hahaidontcarethatyourhousesoldormadeyoulotsofmoney_iusedtarasgadgetstillcouldnotfindwhoasked.top=int(surgegalemrpvssproutjackyemzforworstmetaof2020[1])
-        screen.blit(textdisplay,hahaidontcarethatyourhousesoldormadeyoulotsofmoney_iusedtarasgadgetstillcouldnotfindwhoasked)
+        surface.blit(textdisplay,hahaidontcarethatyourhousesoldormadeyoulotsofmoney_iusedtarasgadgetstillcouldnotfindwhoasked)
     def __repr__(self):
         return self.text#you really do be wanting to print those golems
 
 class ravager(object):
-    def __init__(self,string,rect,font,color=(129,214,61),textcolor=(142,155,229),scrollvalue=20):
+    def __init__(self,surface,string,rect,font,color=(129,214,61),textcolor=(142,155,229),scrollvalue=20):
+        self.surface=surface
         self.textboxrect=rect
         self.string=string
         self.font=font
@@ -26,7 +27,7 @@ class ravager(object):
             self.color=color
         self.textcolor=textcolor
         self.scrollvalue=scrollvalue#how many pixels to scroll by each input
-        self.scrolly=int(self.textboxrect.top)#shifts all the text up by this amount of pixels
+        self.scrolly=0#shifts all the text up by this amount of pixels
         self.GOLEMS=[]
         THEKINGx=0#yes it is the blit spot in the box
         THEKINGy=0
@@ -35,6 +36,12 @@ class ravager(object):
         for x in range(len(self.string)):
             self.string[x]=" "+self.string[x]
             kingsize=self.font.size(self.string[x])
+            if (self.string[x][1:]=="\\n" and x<len(self.string)-1):
+                self.GOLEMS.append(golem(line,self.textcolor,(self.textboxrect.left,THEKINGy+self.textboxrect.top),self.font))
+                THEKINGx=0
+                THEKINGy+=kingsize[1]
+                line=""
+                continue
             if (THEKINGx+kingsize[0]>self.textboxrect.width):
                 if (" " not in line):
                     while (self.font.size(line)[0]>self.textboxrect.width):
@@ -59,18 +66,18 @@ class ravager(object):
                 self.scrolly-=self.scrollvalue
             else:
                 print ("read the instructions next time")#what instructions? i don't see any all i see is messy code
-        if (self.scrolly<self.textboxrect.top):#prevent the user from scrolling once they reached the top
-            self.scrolly=self.textboxrect.top
-        elif (self.scrolly>self.GOLEMS[-1].position[1]+self.font.size(self.GOLEMS[-1].text)[1]-self.textboxrect.height):
-            self.scrolly=self.GOLEMS[-1].position[1]+self.font.size(self.GOLEMS[-1].text)[1]-self.textboxrect.height
+        if (self.scrolly<0):#prevent the user from scrolling once they reached the top
+            self.scrolly=0 
+        elif (self.scrolly>self.GOLEMS[-1].position[1]+self.font.size(self.GOLEMS[-1].text)[1]-self.textboxrect.height-self.textboxrect.top):
+            self.scrolly=self.GOLEMS[-1].position[1]+self.font.size(self.GOLEMS[-1].text)[1]-self.textboxrect.height-self.textboxrect.top
     def display(self):
-        pygame.draw.rect(screen,self.color,self.textboxrect)
+        pygame.draw.rect(self.surface,self.color,self.textboxrect)
         for x in range(len(self.GOLEMS)):
             if (self.GOLEMS[-1].position[1]+self.font.size(self.GOLEMS[x].text)[1]-self.textboxrect.top>self.textboxrect.height):#multiple lines
-                if (self.GOLEMS[x].position[1]>=self.scrolly) and (self.GOLEMS[x].position[1]+self.font.size(self.GOLEMS[x].text)[1]<=self.scrolly+self.textboxrect.height):
-                    self.GOLEMS[x].KING((self.GOLEMS[x].position[0],self.GOLEMS[x].position[1]-(self.scrolly-self.textboxrect.top)))
+                if (self.GOLEMS[x].position[1]>=self.scrolly+self.textboxrect.top) and (self.GOLEMS[x].position[1]+self.font.size(self.GOLEMS[x].text)[1]<=self.scrolly+self.textboxrect.bottom):
+                    self.GOLEMS[x].KING(self.surface,(self.GOLEMS[x].position[0],self.GOLEMS[x].position[1]-(self.scrolly)))
             else:#one line
-                self.GOLEMS[x].KING((self.GOLEMS[x].position[0],self.GOLEMS[x].position[1]))
+                self.GOLEMS[x].KING(self.surface,(self.GOLEMS[x].position[0],self.GOLEMS[x].position[1]))
 
 if (__name__=="__main__"):
     pygame.init()
@@ -80,12 +87,12 @@ if (__name__=="__main__"):
     pygame.display.set_caption("Brawl Ball > Heist > Siege > Gem Grab > Bounty")
     font1 = pygame.font.Font("KINGFONT.ttf",48)
     font2 = pygame.font.Font("KINGFONT.ttf",36)
-    string = "Spring Trap Olive Branch Open Business Warped Arena Minecart Madness Cobweb Sandy Gems Shoulder Bash Deep Diner Junk Park Parallel Plays Well Cut Double Swoosh Canal Grande Tornado Ring Triple Dribble Bouncing Diner Fenced In Split Pinball Dreams Crystal Arcade Snake Prairie Does Anyone Remember How Terrible The 6 Week Cycle Map Rotation Was Imagine Only Playing Snake Prairie Every 3 Weeks Crossroads Center Field Chill Space Some Assembly Required The One Fat Pam Who Assigned This Is Definitely Sprout Ja-- Wait That Was A Long Time Ago Now That One Fat Pam is Definitely Surge Gale Mr.P Very Unplayable Terrible No Good Very Bad Tiny Town Pinhole Punt Cell Division Deeper Danger Bandit Cove Beach Ball Stone Fort Crated Factory Massive Attack Field Goal Hard Rock Mine Heat Wave Hot Potato Super Stadium Undermine Nuts & Bolts Control Grande Post Haste Sorry For The Interruption If You Count The Exact Number Of Words Here I Will Bid On Whack A King On GitHub Deathcap Trap Land Ahoy Traffic Jam Backyard Bowl Spare Space Factory Rush Perimeter Penalty Kick Escape Velocity Layer Cake Pit Stop Galaxy Arena Depression In Season Reset Golem King King Golem Wolf"
+    string = "\\n week 1 \\n Spring Trap Olive Branch Open Business Warped Arena \\n Minecart Madness Cobweb Sandy Gems Shoulder Bash \\n Deep Diner Junk Park Parallel Plays Well Cut \\n Double Swoosh Canal Grande Tornado Ring Triple Dribble \\n Bouncing Diner Fenced In Split Pinball Dreams \\n Crystal Arcade Snake Prairie Crossroads Center Field \\n Chill Space Some Assembly Required Tiny Town Pinhole Punt \\n \\n week 2 \\n Cell Division Deeper Danger Bandit Cove Beach Ball \\n Stone Fort Crated Factory Massive Attack Field Goal \\n Hard Rock Mine Heat Wave Hot Potato Super Stadium \\n Undermine Nuts & Bolts Control Grande Post Haste \\n Deathcap Trap Land Ahoy Traffic Jam Backyard Bowl \\n Spare Space Factory Rush Perimeter Penalty Kick \\n Escape Velocity Layer Cake Pit Stop Galaxy Arena \\n Play Play King Bolem Wolf"
     #string="bolem ging sfsdjfklwejlkrjweirjwelkrjweklrkabcdefghijklmnopqrstuvwxyz gj"
     string2 = "a a a a aa  a a a a aa k k k k k k k k k kl l l l l l l l l 4 4 4 4 4 4 5 5 5 5 55 5 6 6 6 6 6 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 ^ 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9 6 9"
 
-    textboxrect=ravager(string2,pygame.Rect(50,50,300,500),font1,color=(255,3,204),textcolor=(85,255,85),scrollvalue=60)#create instance of ravager
-    textboxrect2=ravager(string,pygame.Rect(650,100,300,750),font2,scrollvalue=40)
+    #textboxrect=ravager(screen,string2,pygame.Rect(50,50,300,500),font1,color=(255,3,204),textcolor=(85,255,85),scrollvalue=60)#create instance of ravager
+    textboxrect2=ravager(screen,string,pygame.Rect(150,100,650,450),font2,scrollvalue=40)
 
     done=False
     # main program loop
@@ -97,14 +104,14 @@ if (__name__=="__main__"):
             
             if event.type==pygame.MOUSEBUTTONDOWN:
                 if event.button==4:
-                    textboxrect.scroll("up",pygame.mouse.get_pos())#scroll 20 pixels per spin
+                    #textboxrect.scroll("up",pygame.mouse.get_pos())#scroll 20 pixels per spin
                     textboxrect2.scroll("up",pygame.mouse.get_pos())#scroll 20 pixels per spin
                 elif event.button==5:
-                    textboxrect.scroll("down",pygame.mouse.get_pos())
+                    #textboxrect.scroll("down",pygame.mouse.get_pos())
                     textboxrect2.scroll("down",pygame.mouse.get_pos())
             
         screen.fill((0,0,0))
-        textboxrect.display()
+        #textboxrect.display()
         textboxrect2.display()
         
         
